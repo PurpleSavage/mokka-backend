85b85594bf49593fff2468683c0bf35025ee0624
PurpleSavage <jzuritapalomino@gmail.com>
2025-04-08 02:20:48 -0500

    agregar capa de servicios externos

--- src/domain/datasources/user.datasource.ts
+++ src/domain/datasources/user.datasource.ts
@@ -6,5 +6,5 @@ import { TextEntity } from "../entities/text.entity";
 
 export abstract class UserDataSource{
     abstract textProofreader(textProofreaderDto: TextProofreaderDto):Promise<TextEntity>
-    abstract audioGeneration():Promise<string>
+    abstract audioGeneration():Promise<string>  
 }
