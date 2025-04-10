2c9ef9569216f482a27995aa8cc4ce2bcb77a268
PurpleSavage <jzuritapalomino@gmail.com>
2025-04-09 20:52:04 -0500

    agregar middleware para refreshtoken y validaciones extras

--- src/presentation/middleware/refreshToken.middleware.ts
+++ src/presentation/middleware/refreshToken.middleware.ts
@@ -1,13 +1,15 @@
 import { Response,Request, NextFunction } from "express";
 import { JWT } from "../../config/jwt";
+import { UserModel } from "../../data/mongodb/models/user.model";
 
 export class RefreshTokenMiddleware{
     static ValidateRefreshToke =async (req:Request,res:Response,next:NextFunction)=>{
-        const refreshToken=req.cookies.refreshToken
+        const refreshToken=req.cookies.refresh_token
         try {
-            const payload =await JWT.validateToken<{id:string}>(refreshToken )
+            const payload =await JWT.validateToken<{email:string}>(refreshToken )
             if(!payload) return res.status(403).json({error:'Invalid session', redirectTo: '/auth',refresh:true})
-            console.log(payload.id)
+            const user = await UserModel.findOne({email:payload.email})
+            if(user?.email !== payload.email) return res.status(403).json({error:'Invalid session', redirectTo: '/auth',refresh:true})
             next()
         } catch (error) {
             console.log(error)
