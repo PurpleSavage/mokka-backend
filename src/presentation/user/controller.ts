1d149da51e3b76a730861a3ccd5552f5426d63d2
PurpleSavage <jzuritapalomino@gmail.com>
2025-04-21 00:23:14 -0500

    correción de bugs en getPRofile

--- src/presentation/user/controller.ts
+++ src/presentation/user/controller.ts
@@ -1,10 +1,6 @@
-import { TextProofreaderDto } from "../../domain/dtos/mediacreator/text-proofreader.dto"
 import { CustomError } from "../../domain/errors/custom.error"
 import { Request, Response } from "express"
-import { TextProofreader } from "../../domain/use-cases/mediacreator/text-proofreader.use-case"
 import { UserRepository } from "../../domain/repositories/user.repository"
-import { AudiogenerationDto } from "../../domain/dtos/mediacreator/audio-generation.dto"
-import { AudioGeneration } from "../../domain/use-cases/mediacreator/audio-generation.use-case"
 import { GetProfileDto } from "../../domain/dtos/user/get-profile.dto"
 import { GetProfile } from "../../domain/use-cases/user/get-profile.dto"
 
@@ -24,7 +20,7 @@ export class UserController{
     getProfile=(req:Request,res:Response)=>{
      
         const [error,getProfileDto]=GetProfileDto.create({ id: req.body._id })
-        console.log(getProfileDto)
+
         if(error){
             return res.status(400).json({error})
         } 
